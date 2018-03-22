353a3e6717fe32cc80418d0322f68a7112df5b8a
yanliang <yanliang@kongfz.com>
2018-03-22 14:20:02 +0800

    ++

--- app.js
+++ app.js
@@ -74,7 +74,7 @@ if (app.get('env') === 'development') {
     });
 }
 
-// production error handler
+// production error handler..
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
     res.status(err.status || 500);
